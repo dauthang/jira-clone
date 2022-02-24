6371020f3df12af74385d4730491751ff423fa16
thangdt@vmodev.com <thangdt@vmodev.com>
2022-02-24 17:10:20 +0700

    update layout

--- src/app/core/layouts/main-layout/main-layout.component.ts
+++ src/app/core/layouts/main-layout/main-layout.component.ts
@@ -1,11 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+
+const listMenu = [
+  {label: "Dashboards", child: [], isDisplay: true},
+  {label: "Projects", child: [], isDisplay: true},
+  {label: "Issues", child: [], isDisplay: true},
+  {label: "Tempo", child: [], isDisplay: true},
+  {label: "Boards", child: [], isDisplay: true},
+  {label: "BigGantt", child: [], isDisplay: true},
+]
+
 @Component({
   selector: 'app-main-layout',
   templateUrl: './main-layout.component.html',
   styleUrls: ['./main-layout.component.scss'],
 })
 export class MainLayoutComponent implements OnInit {
-  constructor() {}
+  listMenu = listMenu;
 
-  ngOnInit(): void {}
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
 }
